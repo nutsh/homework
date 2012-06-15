9b80cb44b55187b762ca96c848ae24860e4d7c0c
blade <luyongdai2011@gmail.com>
2012-06-15 22:07:30 +0800

    chengfabiao

--- src/in/nutsh/ex001/Chengfabiao.java
+++ src/in/nutsh/ex001/Chengfabiao.java
@@ -12,7 +12,18 @@ public class Chengfabiao {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   	 for (int m = 1; m < 10; m++) {
+			for (int n = 1; n <= m; n++) {
+
+					int result = n*m;
+					System.out.print(" " + n + "*" + m + "=" + result);
+
+				}
+				System.out.print("\n");
+
+			
+
+		}
 
 	}
 
